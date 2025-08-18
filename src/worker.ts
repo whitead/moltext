63231c0fa02ce72d2db6d577c22a9bfb42d52e6e
Andrew White <white.d.andrew@gmail.com>
2025-08-17 18:53:53 -0700

    added missing utf-8 slash

--- src/worker.ts
+++ src/worker.ts
@@ -87,7 +87,7 @@ function parseV2000Molblock(mb: string): {
 }
 
 class CharSet {
-  h_single = "─"; v_single = "│"; slash = "/"; backslash = "╲";
+  h_single = "─"; v_single = "│"; slash = "╱"; backslash = "╲";
   h_double = "═"; v_double = "║"; h_triple = "≡";
   static ascii(): CharSet {
     const c = new CharSet();
@@ -306,7 +306,7 @@ export default {
     try {
       if (!mol.has_coords()) mol.set_new_coords(true);
       const molblock = mol.get_molblock();
-      const art = new AsciiMolDrawer(1.0, url.searchParams.get("ascii") !== "1").drawMolblock(molblock);
+      const art = new AsciiMolDrawer(2.0, url.searchParams.get("ascii") !== "1").drawMolblock(molblock);
       let responseContent;
       if (echoSmi) {
         responseContent = `${smi}\n${art}\n`;
